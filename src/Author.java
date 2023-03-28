cf5f8bf7a966115ea6d88e4e4bf158fb3a0a2dd0
eremi <ereminr83@mail.ru>
2023-03-28 22:09:04 +0700

    Second commit

---
+++ src/Author.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+
+public class Author {
+    private String name;
+    private String surname;
+
+
+    public Author(String name, String surname) {                         // можно this (title,author, year);
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+
+        return this.name;
+    }
+
+    public String getSurname() {
+
+        return this.surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор книги: " + name + surname + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && surname.equals(author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+    public static void print() {
+        System.out.println("--------------------");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        System.out.println();
+
+        Author pushkin = new Author("Александр ", "Пушкин");
+        Book dubrovsky = new Book("Дубровский", pushkin, 1833);
+
+        System.out.println(dubrovsky);
+        System.out.println(pushkin);
+
+        print();
+
+        dubrovsky.setYear(1841);
+        System.out.println(dubrovsky);
+        print();
+
+        Author gogol = new Author("Николай ", "Гоголь");
+        Book viy = new Book("Вий", gogol, 1835);
+
+        System.out.println(viy);
+        System.out.println(gogol);
+        print();
+        System.out.println(dubrovsky.equals(viy));
+        System.out.println(pushkin.equals(gogol));
     }
 }
